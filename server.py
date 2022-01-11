91d0d5339f6920613d9a021195319d913ad8c7e9
Koji Tashiro <koji.t@sprobe.com>
2022-01-11 19:43:55 +0900

    Support new Lobe

--- server.py
+++ server.py
@@ -1,22 +1,30 @@
 import io
+import os
 import json
 from flask import Flask, render_template, request
-from lobe import ImageModel
 from PIL import Image
 
-app = Flask(__name__)
-model = ImageModel.load('Fruits TensorFlow')
+from tf_model_helper import TFModel
 
-prices = {'Apple': 99, 'Banana': 88, 'Grapefruit': 128, 'Orange': 66}
+app = Flask(__name__)
+
+# Path to signature.json and model file
+ASSETS_PATH = os.path.join(".", "./model")
+TF_MODEL = TFModel(ASSETS_PATH)
+PRICES = {'Apple': 99, 'Banana': 88, 'Grapefruit': 128, 'Orange': 66}
+
 
 def predict_pil(pil):
-    result = model.predict(pil)
+    result = TF_MODEL.predict(pil)
 
-    #print(result.prediction)
-    for label, prop in result.labels: # Print all classes
-        print(f"{label}: {prop*100}%")
+    #print(result['predictions'])
+    predictions = {}
+    for p in result['predictions']:
+        print(f"{p['label']}: {p['confidence']*100}%")
+        predictions[p['label']] = p['confidence']*100
 
-    return result.as_dict()
+    maxLabel = max((v, k) for k, v in predictions.items())[1]
+    return maxLabel
 
 
 @app.route('/')
@@ -28,10 +36,11 @@ def index():
 def predict():
     img = request.files['image'].read()
     pil = Image.open(io.BytesIO(img))
-    ret = predict_pil(pil)
-    ret['price'] = prices[ret['Prediction']]
-    return json.dumps(ret)
+    label = predict_pil(pil)
+    return json.dumps({'prediction': label, 'price': PRICES[label]})
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    #app.run(debug=True, host='0.0.0.0', port=5000)
+    #app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(debug=True, port=5000)
